8701a1dd9f860d5451639c37b3f4f63663855eea
kimseongjae <stwin755@gmail.com>
2024-04-20 15:39:08 +0900

    v3 ORM 로그인 & 로그아웃

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,11 +1,18 @@
 package shop.mtcoding.blog.user;
 
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final UserRepository userRepository;
+    private final HttpSession session;
 
+// GetMapping
     @GetMapping("/join-form")
     public String joinForm() {
         return "user/join-form";
@@ -21,8 +28,19 @@ public class UserController {
         return "user/update-form";
     }
 
+    // 로그아웃
     @GetMapping("/logout")
     public String logout() {
+        session.invalidate();
+        return "redirect:/";
+    }
+
+// PostMapping
+    // 로그인
+    @PostMapping("/login")
+    public String login(UserRequest.LoginDTO reqDTO) {
+        User sessionUser = userRepository.findByUsernameAndPassword(reqDTO.getUsername(), reqDTO.getPassword());
+        session.setAttribute("sessionUser", sessionUser);
         return "redirect:/";
     }
 }
