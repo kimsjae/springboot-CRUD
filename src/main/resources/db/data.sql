cd57378aae3ea8a8a2040b3ee4304502c3664a24
kimseongjae <stwin755@gmail.com>
2024-04-20 15:18:54 +0900

    [fix] board_tb 더미 수정 - user_id 추가

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -2,7 +2,7 @@ insert into user_tb(username, password, email, created_at) values('ssar', '1234'
 insert into user_tb(username, password, email, created_at) values('cos', '1234', 'cos@nate.com', now());
 insert into user_tb(username, password, email, created_at) values('love', '1234', 'love@nate.com', now());
 
-insert into board_tb(title, content, created_at) values('제목1','내용1',now());
-insert into board_tb(title, content, created_at) values('제목2','내용2',now());
-insert into board_tb(title, content, created_at) values('제목3','내용3',now());
-insert into board_tb(title, content, created_at) values('제목4','내용4',now());
+insert into board_tb(title, content, user_id, created_at) values('제목1','내용1',1,now());
+insert into board_tb(title, content, user_id, created_at) values('제목2','내용2',1,now());
+insert into board_tb(title, content, user_id, created_at) values('제목3','내용3',2,now());
+insert into board_tb(title, content, user_id, created_at) values('제목4','내용4',3,now());
